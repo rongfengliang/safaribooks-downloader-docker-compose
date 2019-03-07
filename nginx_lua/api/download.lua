29216fadb327d2fe71fea12d9589032fe0ad2e56
rongfl <rongfl>
2019-03-07 23:49:40 +0800

    add shell call func

--- nginx_lua/api/download.lua
+++ nginx_lua/api/download.lua
@@ -5,20 +5,20 @@ local json = require("cjson")
 local shell = require "resty.shell"
 function init()
     ngx.req.read_body()
-    local body = ngx.req.get_body_data()
-    if not body then
-        if ngx.req.get_body_file() then
-            return nil, "request body did not fit into client body buffer, consider raising 'client_body_buffer_size'"
-        else
-            return ""
-        end
-    end
-    local downloadinfo = json.decode(body)
+    -- local body = ngx.req.get_body_data()
+    -- if not body then
+    --     if ngx.req.get_body_file() then
+    --         return nil, "request body did not fit into client body buffer, consider raising 'client_body_buffer_size'"
+    --     else
+    --         return ""
+    --     end
+    -- end
+    -- local downloadinfo = json.decode(body)
     local stdin = "hello"
     local timeout = 300000  -- ms
     local max_size = 409600  -- byte
     local ok, stdout, stderr, reason, status =
-        shell.run([[ls /]], stdin, timeout, max_size)
+        shell.run([[ls ]], stdin, timeout, max_size)
     if not ok then
         return ""
     end
